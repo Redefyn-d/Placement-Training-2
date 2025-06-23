8cbdd47886a35a8ef43b933a22708a6f97de5311
Raj <148458558+Redefyn-d@users.noreply.github.com>
2025-06-23 21:01:09 +0530

    Create SP.java

---
+++ 22.06.2025/SP.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class SpiralMatrix {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int rows = input.nextInt();
+        int cols = input.nextInt();
+        int[][] matrix = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = input.nextInt();
+            }
+        }
+        ArrayList<Integer> result = new ArrayList<>();
+        int top = 0, bottom = rows - 1, left = 0, right = cols - 1;
+        while (top <= bottom && left <= right) {
+            for (int j = left; j <= right; j++) {
+                result.add(matrix[top][j]);
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                result.add(matrix[i][right]);
+            }
+            right--;
+            if (top <= bottom) {
+                for (int j = right; j >= left; j--) {
+                    result.add(matrix[bottom][j]);
+                }
+                bottom--;
+            }
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    result.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+        input.close();
+    }
+}
